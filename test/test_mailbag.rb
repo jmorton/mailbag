9640a4db94776d0d6b487e216bdcb9739b432152
Jon Morton <jmorton@customink.com>
2013-06-07 15:42:52 -0400

    Contrive a test.
    …I'm getting my feet wet with this stuff.

--- test/test_mailbag.rb
+++ test/test_mailbag.rb
@@ -6,5 +6,15 @@ require 'debugger'
 class MailbagTest < Minitest::Test
 
   require 'mailbag'
+  require 'mail'
+
+  def mail
+    Mail.read_from_string(open('./test/mail.txt').read)
+  end
+
+  def test_text_to_mail
+    puts mail.from
+    assert mail.from, 'jon.morton@gmail.com'
+  end
 
 end
