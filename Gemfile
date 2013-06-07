0fe0e1c4f75c4bc9be85f51c1290a18c324ce16a
Jon Morton <jmorton@customink.com>
2013-06-07 10:46:28 -0400

    Generalize.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source "https://rubygems.org"
 gem 'eventmachine'
 gem 'em-imap'
 gem 'foreman'
-gem 'messaging_helper', git: 'git@github.com:customink/messaging_helper.git'
 
 group :development do
   gem 'minitest'
--- lib/boot.rb
+++ lib/boot.rb
@@ -2,10 +2,5 @@ require 'messaging_helper'
 require 'mailbag'
 
 Mailbag.run do |message|
-  begin
-    MH.publish 'mailbag', { rfc822: message }, 'tbd'
-  rescue RuntimeError => e
-    Mailbag.logger(e)
-    Mailbag.logger(message)
-  end
+  puts message
 end
